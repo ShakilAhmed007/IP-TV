abb21362731ebb20dbf771844bfb7e4b92117848
SakilAhmed007 <sakilhossain01969@gmail.com>
2021-02-18 00:17:01 +0600

    project fnished

---
+++ js/main.js
@@ -0,0 +1,117 @@
+$('.carousel').carousel({
+    interval: 3000,
+    pause: "false"
+  })
+
+  
+  $(document).ready(function(){
+    $("#gtv").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/07/cwc19.html")+$(".channel-name").text("GTV")
+    })
+
+    $("#sa-tv").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/satv-live.html")+$(".channel-name").text("SA TV")
+    })
+
+    $("#channel24").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/channel24-live.html")+$(".channel-name").text("GTV")
+    })
+
+    $("#n-tv").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/ntv-live.html")+$(".channel-name").text("N TV")
+    })
+
+    $("#desh-tv").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/desh-tv-live.html")+$(".channel-name").text("DESH TV")
+    })
+
+    $("#jamuna-tv").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/jamuna-tv-live.html")+$(".channel-name").text("JAMUNA TV")
+    })
+
+    
+    $("#somoy-tv").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/somoy-tv-live.html")+$(".channel-name").text("SOMOY TV")
+    })
+
+    $("#boisakhi-tv").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/boisakhi-tv-live.html")+$(".channel-name").text("BOISAKHI TV")
+    })
+
+    $("#star-pluse").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/star-plus-live.html")+$(".channel-name").text("STAE PLUSE")
+    })
+
+
+    $("#star-world").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/star-world-live.html")+$(".channel-name").text("STAR WORLD")
+    })
+
+
+    $("#colors-bangla").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/colors-bangla-live.html")+$(".channel-name").text("COLORS BANGLA")
+    })
+
+
+    $("#colors-hd").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/colors-hd-live.html")+$(".channel-name").text("COLORS HD")
+    })
+
+
+    $("#channel-i").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/channel-i-live.html")+$(".channel-name").text("CHANNEL I")
+    })
+
+    $("#atn-bangla").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/atn-bangla-live.html")+$(".channel-name").text("ATN BANGLA")
+    })
+
+    $("#atn-news").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/atn-news-live.html")+$(".channel-name").text("ATN NEWS")
+    })
+
+
+    $("#rtv").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/rtv-live.html")+$(".channel-name").text("RTV")
+    })
+
+
+    $("#ekattor-tv").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/ekattor-tv-live.html")+$(".channel-name").text("EKATTOR TV")
+    })
+
+    $("#bbc-news").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/bbc-world-live.html")+$(".channel-name").text("BBC NEWS")
+    })
+
+    $("#bbc-life").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/bbc-lifestyle-live.html")+$(".channel-name").text("BBC LIFE")
+    })
+
+    $("#dbc-news").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/dbc-news-live.html")+$(".channel-name").text("DBC NEWS")
+    })
+
+
+    $("#nick").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/nick-live.html")+$(".channel-name").text("NICK")
+    })
+
+
+    $("#dipto-tv").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/dipto-tv-live.html")+$(".channel-name").text("DIPTO TV")
+    })
+
+    $("#gaan-bangla").click(function(){
+      $(".skshakil").attr("src", "https://ahstream.blogspot.com/2019/12/gaan-bangla-live.html")+$(".channel-name").text("GAAN BANGLA")
+    })
+
+
+
+
+
+    
+  })
+
+
+ 
